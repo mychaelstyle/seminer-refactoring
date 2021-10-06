48cebd74c31295a2e2bab2c3a4667f80a70b45f2
yujiro-arai <arai_yujiro@giginc.co.jp>
2021-10-06 17:14:15 +0900

    バリデーションを追加

--- public/index.php
+++ public/index.php
@@ -1,27 +1,45 @@
 <?php
-  const DB_FILE_PATH = '../storage/db.csv';
-  $db = file_get_contents(DB_FILE_PATH);
-  $lines = explode("\n", $db);
-  $messages = array_map(function ($line) {
-    if (strlen($line) === 0) {
-      return null;
+    session_start();
+    const DB_FILE_PATH = '../storage/db.csv';
+    $db = file_get_contents(DB_FILE_PATH);
+    $lines = explode("\n", $db);
+    $messages = array_map(function ($line) {
+        if (strlen($line) === 0) {
+            return null;
+        }
+        list($name, $message) = explode(',', $line);
+        return ["name" => $name, "message" => $message];
+    }, $lines);
+
+    $error = null;
+
+    if (isset($_SESSION['error'])) {
+        $error = $_SESSION['error'];
+        unset($_SESSION['error']);
     }
-    list($name, $message) = explode(',', $line);
-    return ["name" => $name, "message" => $message];
-  }, $lines);
 ?>
-<h1>チャット</h1>
-<ul>
-  <?php foreach ($messages as $message) { ?>
-    <?php if ($message !== null) { ?>
-      <li><?php echo $message['name'];?>: <?php echo $message['message']; ?></li>
+<html>
+  <head>
+    <title>チャット！！</title>
+  </head>
+  <body>
+    <h1>チャット</h1>
+    <?php if ($error !== null) { ?>
+        <div style="color: red;"><?php echo $error; ?></div>
     <?php } ?>
-  <?php } ?>
-</ul>
-<form method="post" action="/sendMessage.php">
-  お名前
-  <input name="user" type="text" value="" placeholder="あなたのお名前">
-  発言
-  <input name="message" type="text" value="" placeholder="メッセージ" />
-  <button type="submit">送信</button>
-</form>
+    <ul>
+      <?php foreach ($messages as $message) { ?>
+        <?php if ($message !== null) { ?>
+          <li><?php echo $message['name'];?>: <?php echo $message['message']; ?></li>
+        <?php } ?>
+      <?php } ?>
+    </ul>
+    <form method="post" action="/sendMessage.php">
+      お名前
+      <input name="user" type="text" value="" placeholder="あなたのお名前">
+      発言
+      <input name="message" type="text" value="" placeholder="メッセージ" />
+      <button type="submit">送信</button>
+    </form>
+  </body>
+</html>
--- public/sendMessage.php
+++ public/sendMessage.php
@@ -1,13 +1,52 @@
 <?php
 
+session_start();
+
+$result = validate();
+
+if (!$result) {
+    header('Location: /');
+    saveErrorMessage('ユーザー名とメッセージは必ず設定してください');
+    exit();
+}
+
 $name = $_POST['user'];
 $message = $_POST['message'];
 
-var_dump([$name, $message]);
-
-
 const DB_FILE_PATH = '../storage/db.csv';
+
 $db = file_get_contents(DB_FILE_PATH);
 file_put_contents(DB_FILE_PATH, $db . "{$name},{$message}\n");
 
 header('Location: /');
+
+/**
+ * バリデーション
+ *
+ * @return boolean if true 正常
+ */
+function validate() {
+    if (!isset($_POST['user']) || !isset($_POST['message'])) {
+        header('Location: /');
+        saveErrorMessage('ユーザー名とメッセージは必ず設定してください');
+        return false;
+    }
+
+    if (empty($_POST['user']) || empty($_POST['message'])) {
+        header('Location: /');
+        saveErrorMessage('ユーザー名とメッセージは必ず設定してください');
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * Undocumented function
+ *
+ * @param string $message エラーメッセージ
+ * @return void
+ */
+function saveErrorMessage(string $message) {
+    $_SESSION['error'] = $message;
+}
